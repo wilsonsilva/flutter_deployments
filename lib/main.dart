3767e4abc919c81932401dc0079c3d27d2ed602d
Wilson Silva <wilson.dsigns@gmail.com>
2023-05-06 14:35:54 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterDeployments());
+}
+
+class FlutterDeployments extends StatelessWidget {
+  const FlutterDeployments({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Github Actions Deployments',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+      )
+    );
+  }
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_deployments/main.dart';
+
+void main() {
+  testWidgets('Displays a message', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const FlutterDeployments());
+
+    // Verify that the text is present.
+    expect(find.text('Flutter Github Actions Deployments'), findsOneWidget);
+  });
+}
